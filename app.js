19ff932c4cae05a2102aa5b94d7fcdcdcfe73709
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-04-21 14:45:22 +0200

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
 const moviesRoutes = require('./routes/movies')
 const notFound = require('./middleware/notFound')
 const errorsHandler = require('./middleware/errorsHandler')
 
+app.use(cors()) /* CORS una volta installato evita problemi di sicurezza del browser con chiamate sulla stessa macchina */
 app.use(express.json()) /* body parser */
 app.use(moviesRoutes)
 app.use('/static', express.static('public'))
